322bb60ec1b70c8c6694318c59991e183c926942
meriadec <meriadec.pillet@gmail.com>
2017-01-07 22:52:06 +0100

    start funny things

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react'
+import {
+  HuePicker,
+} from 'react-color'
 
 import Logo from 'components/Logo'
 
 class Home extends Component {
 
+  state = {
+    color: '#ffca2b',
+  }
+
+  handleChangeHue (color) {
+    if (!color) { return }
+
+    const {
+      hex,
+    } = color
+
+    this.setState({
+      color: hex,
+    })
+
+  }
+
   render () {
+
+    const {
+      color,
+    } = this.state
+
     return (
       <div>
-        <Logo />
+
+        <Logo
+          color={color}
+        />
+
+        <div className='controls'>
+          <HuePicker
+            color={color}
+            onChange={::this.handleChangeHue}
+          />
+          <input
+            type='text'
+            value={color}
+            style={{
+              borderColor: color,
+              color,
+            }}
+          />
+        </div>
+
       </div>
     )
   }
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,10 +6,16 @@ import LogoEyes from 'components/logo/eyes'
 class Logo extends Component {
 
   render () {
+
+    const {
+      color,
+    } = this.props
+
     return (
       <div className='Logo'>
         <div className='Logo-body'>
           <LogoBody
+            headColor={color}
             style={{
               width: 300,
             }}
@@ -17,6 +23,7 @@ class Logo extends Component {
         </div>
         <div className='Logo-eyes'>
           <LogoEyes
+            color={color}
             style={{
               width: 100,
             }}
